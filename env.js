4005552bc632b0da7512ff2a9c5b88a9c60cdaa6
Roman Rogozhnikov <yacut@users.noreply.github.com>
2017-09-08 16:25:02 +0200

    Update env.js

--- env.js
+++ env.js
@@ -1,19 +1,19 @@
 // Replace with the URL where a Kafka REST service is listening
 var clusters = [
     {
+      NAME: "dev",
+      KAFKA_REST: "localhost:8082",
+      MAX_BYTES: "50000",
+      COLOR: "red",
+      RECORD_POLL_TIMEOUT: "500",
+      DEBUG_LOGS_ENABLED: true
+    },
+    {
       NAME: "prod",
       KAFKA_REST: "/api",
       MAX_BYTES: "50000", 	// Sets the default maximum amount of bytes to fetch from each topic
       RECORD_POLL_TIMEOUT: "500",
       COLOR: "#141414", // Optional
-      DEBUG_LOGS_ENABLED: true
-    },
-    {
-      NAME: "dev",
-      KAFKA_REST: "localhost",
-      MAX_BYTES: "50000",
-      COLOR: "red",
-      RECORD_POLL_TIMEOUT: "500",
       DEBUG_LOGS_ENABLED: true
     }
   ];
